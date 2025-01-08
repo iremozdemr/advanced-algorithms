fa2c466af527acb6a20575945494acd3742c3566
İrem Özdemir <irem.ozdemir@etu.edu.tr>
2025-01-08 18:52:02 +0300

    add fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,42 @@
+console.log("hello");
+
+const foo = (n) => {
+    if(n <= 1) return;
+    foo(n-1);
+};
+//O(n) time
+//O(n) space
+
+const bar = (n) => {
+    if(n <= 1) return;
+    bar(n-2);
+};
+//O(n) time
+//O(n) space
+
+const dib = (n) => {
+    if(n <= 1) return;
+    dib(n-1);
+    dib(n-1);
+}
+//O(2^n) time
+//O(n) space
+
+const lib = (n) => {
+    if(n <= 1) return;
+    lib(n-2);
+    lib(n-2);
+}
+//O(2^n) time
+//O(n) space
+
+const fib = (n) => {
+    if(n <= 2) return 1;
+    return fib(n-1) + fib(n-2);
+};
+//O(2^n) time
+//O(n) space
+
+//O(dib) <= O(fib) <= O(lib)
+//O(2^n) <= O(fib) <= O(2^n)
+//O(fib) = O(2^n)
